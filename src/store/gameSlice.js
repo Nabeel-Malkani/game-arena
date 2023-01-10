a94d81f99ba90a172577c72accf38aecd913c862
Muhammad Nabeel Ahmad <nabeel.d@dplit.com>
2023-01-10 13:01:00 +0500

    Initial Commit -- INSERT -- Initial commit

---
+++ src/store/gameSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit"; 
+
+const initialState = {
+    allGames: [],
+    searchGames: [],
+    pcGames: []
+}
+
+export const gameSlice = createSlice(
+    {
+        name: "games",
+        initialState,
+        reducers:{
+            allGames: (state, action)=>{
+                state.allGames = action.payload
+            },
+            searchGames: (state, action)=>{
+                state.searchGames = action.payload
+            },
+            pcGames: (state, action)=>{
+                state.pcGames = action.payload
+            }
+        }
+    }
+);
+
+export const { allGames, searchGames , pcGames } = gameSlice.actions;
+export default gameSlice.reducer;
