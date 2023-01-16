5f986de05267ec8408d63c8e950f1f47e1b35246
Nabeel <nabeelmalkani@gmail.com>
2023-01-16 11:03:45 +0500

    Card Changes --INSERT-- Card Changes

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit"
 import gamesReducer from "./gameSlice"
+import thunk from "redux-thunk"
+import logger from "redux-logger"
 
 export const store = configureStore({
   reducer: {
     games: gamesReducer,
   },
-});
+  middleware: [thunk, logger],
+})
